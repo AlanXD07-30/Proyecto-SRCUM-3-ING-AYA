0d05da627e7c4f27913e4c20c64ac6c10fbe60b9
AlanXD07-30 <alanrioscast2435@gmail.com>
2026-02-17 22:02:48 -0500

    Corrigiendo diseño

--- CRUD/db.py
+++ CRUD/db.py
@@ -9,7 +9,7 @@ def get_conn():
     return psycopg.connect(
         host=os.getenv("DB_HOST", "localhost"),
         port=os.getenv("DB_PORT", "5432"),
-        dbname=os.getenv("DB_NAME", "crud"),  # 👈 nombre actualizado
+        dbname=os.getenv("DB_NAME", "crud"),  
         user=os.getenv("DB_USER", "postgres"),
         password=2426022023,
         row_factory=dict_row,
